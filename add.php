d4a415ee86911ccd8631fd497258c50ba4a9b29e
Arturo <dreikuoman@gmail.com>
2022-11-28 16:05:01 +0100

    Validation & bind parameters

--- add.php
+++ add.php
@@ -1,15 +1,28 @@
 <?php
 require "database.php";
 //Si la variable global $SERVER contiene el methodo POST, guarda en el array $contact el valor de "name" del la variable globarl $_POST
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $name = $_POST["name"];
-    $phoneNumber = $_POST["phone_number"];
 
-    $statement = $conn->prepare("INSERT INTO contacts (name, phone_number) VALUES ('$name', '$phoneNumber')");
-    $statement->execute();
-  
-    //Redirige a index.php
-    header("location: index.php");
+$error = null;
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    if(empty($_POST["name"]) && empty($_POST["phone_number"])){
+      $error = "Please fill all fields";
+    } else if (strlen($_POST["phone_number"]) < 9){
+      $error = "Phone number must be at least 9 character";
+    } else {
+      $name = $_POST["name"];
+      $phoneNumber = $_POST["phone_number"];
+
+      $statement = $conn->prepare("INSERT INTO contacts (name, phone_number) VALUES (:name, :phone_number)");
+      $statement->bindParam(":name", $_POST["name"]);
+      $statement->bindParam(":phone_number", $_POST["phone_number"]);
+      $statement->execute();
+    
+      //Redirige a index.php
+      header("location: index.php");
+
+    }
+
   
 }
 ?>
@@ -74,12 +87,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
           <div class="card">
             <div class="card-header">Add New Contact</div>
             <div class="card-body">
+              <?php if($error): ?>
+                <p class="text-danger"><?=$error ?></p>
+              <?php endif ?>
               <form method="POST" action="./add.php">
                 <div class="mb-3 row">
                   <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
     
                   <div class="col-md-6">
-                    <input id="name" type="text" class="form-control" name="name" required autocomplete="name" autofocus>
+                    <input id="name" type="text" class="form-control" name="name" autocomplete="name" autofocus>
                   </div>
                 </div>
     
@@ -87,7 +103,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                   <label for="phone_number" class="col-md-4 col-form-label text-md-end">Phone Number</label>
     
                   <div class="col-md-6">
-                    <input id="phone_number" type="tel" class="form-control" name="phone_number" required autocomplete="phone_number" autofocus>
+                    <input id="phone_number" type="tel" class="form-control" name="phone_number" autocomplete="phone_number" autofocus>
                   </div>
                 </div>
     
