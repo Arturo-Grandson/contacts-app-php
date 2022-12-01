845e4bbcd1dd33e5c37953d3752660853e68e290
Arturo <dreikuoman@gmail.com>
2022-12-01 13:32:36 +0100

    Partial hrml

--- add.php
+++ add.php
@@ -1,8 +1,14 @@
 <?php
 require "database.php";
-//Si la variable global $SERVER contiene el methodo POST, guarda en el array $contact el valor de "name" del la variable globarl $_POST
+//Si la variable global $SERVER contiene el methodo POST, guarda en el array $contact el valor de "name" del la variable global $_POST
 
 $error = null;
+/**Si $_POST["name"] o $_POST["phone_number"]  estan vacios, $error  pasa a contener una cadena de texto con el aviso
+ * si $_POST["phone_number"] tiene un length < 9. $error pasa a contener una cadena de texto con el aviso
+ * sino, se guarda "name" y "phone_number",
+ * Se hace una peticion para insertar los valores en la base de datos bindeando los valores $_POST["name"] y $_POST["phone_number]
+ * para evitar injecciones SQL
+*/
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     if(empty($_POST["name"]) && empty($_POST["phone_number"])){
@@ -22,104 +28,48 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
       header("location: index.php");
 
     }
-
-  
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php require "./partials/header.php"?>
+<div class="container pt-5">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">Add New Contact</div>
+        <div class="card-body">
+          <!-- Si $error es distinto de null, significa que su valor es un error, entonces lo muestrs -->
+          <?php if($error): ?>
+            <p class="text-danger">
+              <?=$error ?>
+            </p>
+          <?php endif ?>
+          <form method="POST" action="./add.php">
+            <div class="mb-3 row">
+              <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
 
-  <link 
-    rel="stylesheet" 
-    href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.2.2/darkly/bootstrap.min.css" 
-    integrity="sha512-8RiGzgobZQmqqqJYja5KJzl9RHkThtwqP1wkqvcbbbHNeMXJjTaBOR+6OeuoxHhuDN5h/VlgVEjD7mJu6KNQXA==" 
-    crossorigin="anonymous" 
-    referrerpolicy="no-referrer" />
-  <script
-    defer 
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" 
-    crossorigin="anonymous">
-  </script>
-
-  <link rel="stylesheet" href="./static/css/index.css" />
-
-  <title>Contacts App</title>
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand font-weight-bold" href="#">
-          <img class="mr-2" src="./static/img/logo.png" />
-          ContactsApp
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="./index.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Add Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    <div class="container pt-5">
-      <div class="row justify-content-center">
-        <div class="col-md-8">
-          <div class="card">
-            <div class="card-header">Add New Contact</div>
-            <div class="card-body">
-              <?php if($error): ?>
-                <p class="text-danger"><?=$error ?></p>
-              <?php endif ?>
-              <form method="POST" action="./add.php">
-                <div class="mb-3 row">
-                  <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
-    
-                  <div class="col-md-6">
-                    <input id="name" type="text" class="form-control" name="name" autocomplete="name" autofocus>
-                  </div>
-                </div>
-    
-                <div class="mb-3 row">
-                  <label for="phone_number" class="col-md-4 col-form-label text-md-end">Phone Number</label>
-    
-                  <div class="col-md-6">
-                    <input id="phone_number" type="tel" class="form-control" name="phone_number" autocomplete="phone_number" autofocus>
-                  </div>
-                </div>
-    
-                <div class="mb-3 row">
-                  <div class="col-md-6 offset-md-4">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                  </div>
-                </div>
-              </form>
+              <div class="col-md-6">
+                <input id="name" type="text" class="form-control" name="name" autocomplete="name" autofocus>
+              </div>
             </div>
-          </div>
+
+            <div class="mb-3 row">
+              <label for="phone_number" class="col-md-4 col-form-label text-md-end">Phone Number</label>
+
+              <div class="col-md-6">
+                <input id="phone_number" type="tel" class="form-control" name="phone_number" autocomplete="phone_number" autofocus>
+              </div>
+            </div>
+
+            <div class="mb-3 row">
+              <div class="col-md-6 offset-md-4">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
-    <main>
-
-    </main>
-</body>
-</html>
+  </div>
+</div>
+<?php require "./partials/footer.php" ?>
--- edit.php
+++ edit.php
@@ -10,10 +10,10 @@ if($statement->rowCount() == 0){
   echo("HTTP 404 NOT FOUND");
   return;
 }
-
+//$contact contiene dentro lo que recibe $statement, y con ->fetch(PDO::FETCH_ASSOC), convierte el objeto en un array asociativo
 $contact = $statement->fetch(PDO::FETCH_ASSOC);
 
-//Si la variable global $SERVER contiene el methodo POST, guarda en el array $contact el valor de "name" del la variable globarl $_POST
+//Si la variable global $SERVER contiene el methodo POST, guarda en el array $contact el valor de "name" del la variable global $_POST
 $error = null;
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
@@ -37,103 +37,47 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     }
 
-  
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php require "./partials/header.php" ?>
+<div class="container pt-5">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">Add New Contact</div>
+        <div class="card-body">
+          <?php if($error): ?>
+            <p class="text-danger">
+              <?=$error ?>
+            </p>
+          <?php endif ?>
+          <form method="POST" action="./edit.php?id=<?= $contact["id"]?>">
+            <div class="mb-3 row">
+              <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
 
-  <link 
-    rel="stylesheet" 
-    href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.2.2/darkly/bootstrap.min.css" 
-    integrity="sha512-8RiGzgobZQmqqqJYja5KJzl9RHkThtwqP1wkqvcbbbHNeMXJjTaBOR+6OeuoxHhuDN5h/VlgVEjD7mJu6KNQXA==" 
-    crossorigin="anonymous" 
-    referrerpolicy="no-referrer" />
-  <script
-    defer 
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" 
-    crossorigin="anonymous">
-  </script>
-
-  <link rel="stylesheet" href="./static/css/index.css" />
-
-  <title>Contacts App</title>
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand font-weight-bold" href="#">
-          <img class="mr-2" src="./static/img/logo.png" />
-          ContactsApp
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="./index.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Add Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    <div class="container pt-5">
-      <div class="row justify-content-center">
-        <div class="col-md-8">
-          <div class="card">
-            <div class="card-header">Add New Contact</div>
-            <div class="card-body">
-              <?php if($error): ?>
-                <p class="text-danger"><?=$error ?></p>
-              <?php endif ?>
-              <form method="POST" action="./edit.php?id=<?= $contact["id"]?>">
-                <div class="mb-3 row">
-                  <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
-    
-                  <div class="col-md-6">
-                    <input value="<?= $contact["name"] ?>" id="name" type="text" class="form-control" name="name" autocomplete="name" autofocus>
-                  </div>
-                </div>
-    
-                <div class="mb-3 row">
-                  <label for="phone_number" class="col-md-4 col-form-label text-md-end">Phone Number</label>
-    
-                  <div class="col-md-6">
-                    <input value="<?=$contact["phone_number"]?>" id="phone_number" type="tel" class="form-control" name="phone_number" autocomplete="phone_number" autofocus>
-                  </div>
-                </div>
-    
-                <div class="mb-3 row">
-                  <div class="col-md-6 offset-md-4">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                  </div>
-                </div>
-              </form>
+              <div class="col-md-6">
+                <input value="<?= $contact["name"] ?>" id="name" type="text" class="form-control" name="name" autocomplete="name" autofocus>
+              </div>
             </div>
-          </div>
+
+            <div class="mb-3 row">
+              <label for="phone_number" class="col-md-4 col-form-label text-md-end">Phone Number</label>
+
+              <div class="col-md-6">
+                <input value="<?=$contact["phone_number"]?>" id="phone_number" type="tel" class="form-control" name="phone_number" autocomplete="phone_number" autofocus>
+              </div>
+            </div>
+
+            <div class="mb-3 row">
+              <div class="col-md-6 offset-md-4">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
-    <main>
-
-    </main>
-</body>
-</html>
+  </div>
+</div>
+<?php require "./partials/footer.php" ?>
